fa79d94b0755eaceaa920785f76662e1aa20548e
Gordon Sau <t.sau@student.unsw.edu.au>
2022-07-30 16:55:09 +1000

    implment consumer actor

--- AkkaDemo/src/main/java/org/example/ConsumerActor.java
+++ AkkaDemo/src/main/java/org/example/ConsumerActor.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.example.ProducerConsumer.StartProducerConsumerReqeust;
+import java.math.BigInteger;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
@@ -8,30 +8,47 @@ import akka.actor.typed.javadsl.*;
 
 public class ConsumerActor extends AbstractBehavior<ConsumerActor.Msg> {
     public static interface Msg {}
+    
+    public static class DataMsg implements Msg {
+        public final long data;
+        public DataMsg(long data) {
+            this.data = data;
+        }
+    }
 
     private final ActorRef<BufferActor.BufferCommand> buffer;
 
     private ConsumerActor(ActorContext<Msg> context, ActorRef<BufferActor.BufferCommand> buffer) {
         super(context);
         this.buffer = buffer;
+        requestConsume();
     }
 
     @Override
     public Receive<Msg> createReceive() {
         return newReceiveBuilder()
-          .onMessage(Msg.class, this::receiveMsg)
-          .onMessage(StartProducerConsumerReqeust.class, reqeust -> requestConsume())
+          .onMessage(DataMsg.class, this::receiveMsg)
           .build();
     }
 
-    private Behavior<Msg> receiveMsg(Msg msg) {
-        // TODO
-        return requestConsume();
+    private Behavior<Msg> receiveMsg(DataMsg msg) {
+        proceess(msg.data);
+        requestConsume();
+        return this;
     }
 
-    private Behavior<Msg> requestConsume() {
+    private void requestConsume() {
         buffer.tell(new BufferActor.Consume(getContext().getSelf()));
-        return this;
+    }
+
+    private void proceess(long data) {
+        getContext().getLog().info("Consumer {} is processing data: {}", getContext().getSelf().path(), data);
+        // some complex calculations
+        BigInteger result = BigInteger.ZERO;
+        for (int i = 0; i <= data; i++) {
+            result = result.add(BigInteger.valueOf(i).pow(3));
+        }
+        getContext().getLog().info("Consumer {} completes processing! The result is {}.", getContext().getSelf().path(), result);
     }
 
     public static Behavior<ConsumerActor.Msg> create(ActorRef<BufferActor.BufferCommand> buffer) {
