9bf2fe79eeaa1bac03eb2832eab3e4fd909b044f
Gordon Sau <t.sau@student.unsw.edu.au>
2022-07-31 22:15:17 +1000

    initialize maps

--- AkkaDemo/src/main/java/org/example2/ProducerConsumer.java
+++ AkkaDemo/src/main/java/org/example2/ProducerConsumer.java
@@ -1,5 +1,6 @@
 package org.example2;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.example2.BufferActor.BufferCommand;
@@ -24,8 +25,8 @@ public class ProducerConsumer extends AbstractBehavior<ProducerConsumer.Command>
 
 
     private ActorRef<BufferCommand> buffer;
-    private Map<Long, ActorRef<ProducerActor.Command>> producers;
-    private Map<Long, ActorRef<ConsumerActor.Msg>> consumers;
+    private Map<Long, ActorRef<ProducerActor.Command>> producers = new HashMap<>();
+    private Map<Long, ActorRef<ConsumerActor.Msg>> consumers = new HashMap<>();
 
     public static Behavior<Command> create(long nProducers, long nConsumers, long bufferSize) {
         return Behaviors.setup(context -> new ProducerConsumer(context, nProducers, nConsumers, bufferSize));
