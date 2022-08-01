f9d2eea856d0d70867e0b1e99e7c24f53d580622
Gordon Sau <t.sau@student.unsw.edu.au>
2022-08-01 11:29:25 +1000

    add functions in lib to pretend processing

---
+++ AkkaDemo/src/main/java/org/lib/Processing.java
@@ -0,0 +1,21 @@
+package org.lib;
+
+public class Processing {
+    public static String consuming(String consumer, String data, long timeToProcess) {
+        try {
+            Thread.sleep(timeToProcess);
+        } catch (InterruptedException e) {
+            System.err.printf("%s: Sleep is interrupted\n", consumer);
+        }
+        return data;
+    }
+
+    public static String producing(String producer, long seed, long timeToProcess) {
+        try {
+            Thread.sleep(timeToProcess);
+        } catch (InterruptedException e) {
+            System.err.printf("%s: Sleep is interrupted\n", producer);
+        }
+        return producer + seed;
+    }
+}
