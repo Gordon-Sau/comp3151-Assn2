f907f60530d440e3bd1f063ca9bcb820d2f97837
Gordon Sau <t.sau@student.unsw.edu.au>
2022-08-02 02:18:10 +1000

    implement BoundedBuffer!

--- AkkaDemo/src/main/java/org/pull/BoundedBuffer.java
+++ AkkaDemo/src/main/java/org/pull/BoundedBuffer.java
@@ -1,7 +1,7 @@
 package org.pull;
 
 import java.util.ArrayDeque;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Queue;
 
@@ -10,18 +10,32 @@ import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 
+// this implementation is inspired by the Merge class in Akka Stream, which also
+// uses a buffer for each inputPort
+// we are basically putting a Merge between a buffer and the producers
+// reference: https://github.com/akka/akka/blob/afe4a08133f907066b74d00167888c94936481c8/akka-stream/src/main/scala/akka/stream/scaladsl/Graph.scala?fbclid=IwAR2rgASpa4W7yzb_q49RKL-z9Qq0BLbr-ZfVxhqLzD0HDjDKfyI6X8-E-ds#L89
+
 public class BoundedBuffer extends BufferActor {
+
+    // we are going to use more memory than maxSize in order to keep track of 
+    // the producers and consumers that are ready
 
     // stores the actual data
     private final Queue<String> buffer = new ArrayDeque<>();
-    private final Map<ActorRef<ProducerActor.Command>, String> producerBuffer = new HashMap<>();
-    // keep track of the consumers that are ready for receiving data
-    private final Queue<ActorRef<ConsumerActor.Msg>> consumersQueue = new ArrayDeque<>();
-    // keep track of the producers it can request
-    private final Queue<ActorRef<ProducerActor.Command>> producersQueue = new ArrayDeque<>();
-    private final long maxSize;
-    private long outDeficit = 0; // number of ProduceRequest that have sent out but have not received a response
 
+    // keep track of the producers that have already produced and are 
+    // ready for the next production
+    // we use LinkedHashMap as we also want to keep track of the order
+    private final LinkedHashMap<ActorRef<ProducerActor.Command>, String> 
+        producerBuffer = new LinkedHashMap<>();
+
+    // keep track of the consumers that are ready for receiving data
+    private final Queue<ActorRef<ConsumerActor.Msg>> consumersQueue = new 
+        ArrayDeque<>();
+
+    private final long maxSize;
+
+    /* constructor */
     public static Behavior<BufferActor.BufferCommand> create(long bufferSize) {
         return Behaviors.setup(context -> new BoundedBuffer(context, bufferSize));
     }
@@ -31,85 +45,84 @@ public class BoundedBuffer extends BufferActor {
         this.maxSize = bufferSize;
     }
 
+    // Note:
+    // we will send request to the producer if the producer is ready and
+    // the buffer is not full
+
     @Override
     protected Behavior<BufferCommand> onConsume(Consume request) {
-        // same as unbounded buffer
+        // similar to unbounded buffer
         if (buffer.isEmpty()) {
             consumersQueue.add(request.consumer);
         } else {
-            request.consumer.tell(new ConsumerActor.DataMsg(buffer.poll()));
+            String data = buffer.poll();
+            request.consumer.tell(new ConsumerActor.DataMsg(data));
+            if (!producerBuffer.isEmpty()) {
+                // get data from a producerBuffer to fill up the empty slot in the
+                // buffer and request that producer to produce the next data
+                Map.Entry<ActorRef<ProducerActor.Command>, String> firstEntry = poll(producerBuffer);
+                // nextProducer is ready for the next request
+                ActorRef<ProducerActor.Command> nextProducer = firstEntry.getKey();
+                nextProducer.tell(ProducerActor.RequestProduce.INSTANCE);
+                buffer.add(firstEntry.getValue());
+            }
+            // if producerBuffer is empty, then no producer is ready for the next
+            // request, so we don't send a request
         }
-
-        // request a producer if a consumer consumes from the buffer (2nd case)
-        requestProducersUntilFull();
 
         return this;
     }
 
     @Override
     protected Behavior<BufferCommand> onProduce(Produce request) {
-        outDeficit--;
         if (consumersQueue.isEmpty()) {
-            buffer.add(request.data);
+            if (isBufferFull()) {
+                // the producer is ready, but the buffer is full ->
+                // put it in producerBuffer
+                producerBuffer.put(request.producer, request.data);
+            } else {
+                buffer.add(request.data);
+                // the producer is ready and buffer is not full
+                // send request to the producer
+                request.producer.tell(ProducerActor.RequestProduce.INSTANCE);
+            }
         } else {
+            // send data to a consumer
             consumersQueue.poll().tell(new ConsumerActor.DataMsg(request.data));
+            // buffer is empty (if it is not, then the consumerQueue will be empty)
+            // and the producer is readys
+            request.producer.tell(ProducerActor.RequestProduce.INSTANCE);
         }
-        // Note: 
-        // the loop will only run once if a consumer consumes data (2nd case)
-        requestProducersUntilFull();
         return this;
     }
     
-    private boolean isFull() {
-        return buffer.size() + outDeficit >= maxSize;
+    private boolean isBufferFull() {
+        return buffer.size() >= maxSize;
+    }
+
+    // get the first entry and remove it from the map
+    // Note: LinkedHashMap maintains the insertion order
+    private <K, V> Map.Entry<K,V> poll(LinkedHashMap<K,V> map) {
+        Map.Entry<K,V> firstEntry = map.entrySet().iterator().next();
+        map.remove(firstEntry.getKey());
+        return firstEntry;
     }
 
     @Override
     protected Behavior<BufferCommand> onRegisterProducer(RegisterProducer request) {
-        producersQueue.add(request.producer);
-        // Note: the loop will only run for the first producer
-        // to fill up the buffer
-        requestProducersUntilFull();
+        // the producer must be ready, so the buffer requests the producer
+        request.producer.tell(ProducerActor.RequestProduce.INSTANCE);
         return this;
     }
 
+    // We don't actually need the Finish protocol
+    // Yet, Finish may be useful to distinguish whether the case that the producer
+    // has completed its tasks, or the case that the producer has crashed suddenly
     @Override
     protected Behavior<BufferCommand> onFinish(Finish request) {
-        // do not request the producer anymore
-        outDeficit--; // receive response
-        producersQueue.remove(request.producer);
+        // As long as we don't get a Produce from a producer, we are not going to
+        // request the producer again.
         return this;
     }
 
-    private void requestOneProducer() {
-        if (producersQueue.isEmpty()) {
-            return;
-        }
-        // Use queue to ensure that everyone has the chance to produce 
-        // (eventual entry if every producer response to its request eventually)
-
-        // Drawbacks: It is possible that we are waiting for some producers for 
-        // too long, and the parallelism is lost as other producers needs to wait 
-        // for these producers to produce first before receiving a RequestProduce
-        // and have the chance to produce.
-        ActorRef<ProducerActor.Command> producer = producersQueue.poll();
-        producer.tell(ProducerActor.RequestProduce.INSTANCE);
-        // put the producer back to the end of the queue
-        producersQueue.add(producer);
-        outDeficit++; // create request
-
-        // One may use a different scheduling strategy for better parallelism
-        // e.g. prioirity based on previous throughput,
-        // partition the buffer so that every producer contains a fixed amount of
-        // isolated buffer
-    }
-
-    private void requestProducersUntilFull() {
-        if (producersQueue.isEmpty()) {
-            return;
-        }
-        while (!isFull()) {
-            requestOneProducer();
-        }
-    }
 }
